7f84cffb1aed67b29650e090f6bcc2b8c884aba0
FelesFurva <kitten.solar@gmail.com>
2022-02-24 20:51:13 +0200

    chaned the way age is printed to the cnosole

--- SuperHero Project/Program.cs
+++ SuperHero Project/Program.cs
@@ -1,10 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 string name = "SuperCat";
-int age = 27;
-string power1 = "has 9 lives";
-string power2 = "can fight lazer pointers";
-string power3 = "walks without making a sound";
+int age = 33;
+string power1, power2, power3;
+power1 = "has 9 lives";
+power2 = "can fight lazer pointers";
+power3 = "walks without making a sound";
 
 
 Console.WriteLine("===============================");
@@ -12,5 +13,5 @@ Console.WriteLine("            Hero");
 Console.WriteLine("===============================");
 Console.WriteLine();
 Console.WriteLine($"    Hero Name: {name}");
-Console.WriteLine($"    Hero Age: {age}");
+Console.WriteLine($"    Hero Age: " + age);
 Console.WriteLine($"    Hero Powers: {power1}; {power2}; {power3};");
