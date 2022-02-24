6497914bb746d6ceb5fd16a3f3b996ab8244fa80
FelesFurva <kitten.solar@gmail.com>
2022-02-24 21:06:26 +0200

    Added some decorations

--- SuperHero Project/Program.cs
+++ SuperHero Project/Program.cs
@@ -7,11 +7,12 @@ power1 = "has 9 lives";
 power2 = "can fight lazer pointers";
 power3 = "walks without making a sound";
 
-
-Console.WriteLine("===============================");
+Console.WriteLine(" ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-");
+Console.WriteLine(" ===============================");
 Console.WriteLine("            Hero");
-Console.WriteLine("===============================");
+Console.WriteLine(" ===============================");
 Console.WriteLine();
 Console.WriteLine($"    Hero Name: {name}");
 Console.WriteLine($"    Hero Age: " + age);
 Console.WriteLine($"    Hero Powers: {power1}; {power2}; {power3};");
+Console.WriteLine(" ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-");
