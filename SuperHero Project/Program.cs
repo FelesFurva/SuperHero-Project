85e458a2a1157c6a0cc5cd62a423631633df6eac
FelesFurva <kitten.solar@gmail.com>
2022-02-24 20:13:07 +0200

    First hero card

--- SuperHero Project/Program.cs
+++ SuperHero Project/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string name = "SuperCat";
+int age = 27;
+string power1 = "has 9 lives";
+string power2 = "can fight lazer pointers";
+string power3 = "walks without making a sound";
+
+
+Console.WriteLine("===============================");
+Console.WriteLine("            Hero");
+Console.WriteLine("===============================");
+Console.WriteLine();
+Console.WriteLine($"    Hero Name: {name}");
+Console.WriteLine($"    Hero Age: {age}");
+Console.WriteLine($"    Hero Powers: {power1}; {power2}; {power3};");
